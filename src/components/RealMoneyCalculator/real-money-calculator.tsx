771c408e2dbbaaa42eafbe51ac2d60090f0cc994
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2025-03-26 12:39:12 -0600

    Handle decimals in kks parser

--- src/components/RealMoneyCalculator/real-money-calculator.tsx
+++ src/components/RealMoneyCalculator/real-money-calculator.tsx
@@ -73,13 +73,13 @@ export function RealMoneyCalculator() {
   };
 
   const parseKkString = (stringWithKk: string): number => {
-    const match = stringWithKk.match(/^(\d+)(k+)?$/i);
+    const match = stringWithKk.match(/^([\d.]+)(k+)?$/i);
 
     if (!match) {
       throw new Error("Invalid string format");
     }
 
-    const number = parseInt(match[1], 10);
+    const number = parseFloat(match[1]);
     const kCount = match[2] ? match[2].length : 0;
     const multiplier = Math.pow(1000, kCount);
 
