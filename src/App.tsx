34a700343c7599045363173cb8bde3ff5542bf64
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2025-08-10 22:35:00 -0600

    Work In Progress auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { NavBar } from "@/components/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Pages
-import HomePage from "@/pages/HomePage";
-import RealMoneyPage from "@/pages/RealMoneyPage";
-import ImbuingsPage from "@/pages/ImbuingsPage";
-import HuntingSpotsPage from "@/pages/HuntingSpotsPage";
-import VocationHuntSpotsPage from "@/pages/VocationHuntSpotsPage";
-import ExaltationPage from "@/pages/ExaltationPage";
+import { BrowserRouter } from "react-router-dom";
+import AppRouting from './routes';
+import { Provider } from 'react-redux';
+import { store } from '@/store';
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <NavBar />
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/real-money-calculator" element={<RealMoneyPage />} />
-            <Route path="/imbuings" element={<ImbuingsPage />} />
-            <Route path="/hunting-spots" element={<HuntingSpotsPage />} />
-            <Route
-              path="/hunting-spots/:vocationId"
-              element={<VocationHuntSpotsPage />}
-            />
-            <Route path="/exaltation" element={<ExaltationPage />}></Route>
-          </Routes>
-        </div>
-      </Router>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <BrowserRouter>
+          <NavBar />
+          <div className='flex flex-col items-center justify-center min-h-screen'>
+            <AppRouting />
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
